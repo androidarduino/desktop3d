672cd60ed04bcdb0b179e672c74b9fb10457fbee
vrcats <vrcats@8a642414-8d16-11de-b664-3b115b7b7a9b>
2010-09-24 07:19:04 +0000

    bug found, it is the facingbait problem, looking for a solution.

--- trunk/fishingsrc/release/moc_scene.cpp
+++ trunk/fishingsrc/release/moc_scene.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'scene.h'
 **
-** Created: Tue Sep 14 19:32:01 2010
+** Created: Thu Sep 23 20:15:46 2010
 **      by: The Qt Meta Object Compiler version 61 (Qt 4.5.3)
 **
 ** WARNING! All changes made in this file will be lost!
