866c3a82d66a5739ad927d857f9e4aa3eac6d8d8
vrcats <vrcats@8a642414-8d16-11de-b664-3b115b7b7a9b>
2011-01-07 12:18:46 +0000

    made it running

--- trunk/flags/flag.cpp
+++ trunk/flags/flag.cpp
@@ -1,19 +1,4 @@
-<html><head><title>SimCloth.zip ClothMesh.cpp</title>
-<LINK href="/inc/read_style.css" type=text/css rel=stylesheet></head>
-<body>
-<p><a href=http://www.pudn.com>www.pudn.com</a> &gt; <a href="http://www.pudn.com/downloads78/sourcecode/graph/detail298391.html">SimCloth.zip</a> &gt ClothMesh.cpp, change:2006-11-28,size:13401b</p><!-- saved from http://www.pudn.com -->
-<script src="/inc/gg_read1.js"></script><BR>
-<pre name="code" class="cpp">
-/* ========================================================================= * 
- *	Name:		ClothMesh.cpp                                                * 
- *	Function:	Implement class ClothMesh.                                   * 
- *	Date:		2006-11-21                                                   * 
- *	Editor:		jixing_alex                                                  * 
- * ========================================================================= */ 
- 
-#include "ClothMesh.h" 
- 
-// Refresh point's data in cloth's mesh after interval 
+#include "flag.h" 
 void ClothMesh::DealMesh(void) 
 { 
 	int i, j; 
@@ -26,7 +11,6 @@ void ClothMesh::DealMesh(void)
 	fWidth = WIDTH / (SIZEU - 1); 
 	fLength = LENGTH / (SIZEV - 1); 
 	fCross = sqrt(fWidth * fWidth + fLength * fLength); 
- 
 	// Computer every point's composition of forces, position and speed 
 	for(i = 0; i < SIZEV; i++) 
 	{ 
@@ -38,7 +22,6 @@ void ClothMesh::DealMesh(void)
 			m_vSpeed[i][j] += vForce[i][j] * FTIME / MASS * 0.01f; 
 		} 
 	} 
- 
 	// Computer every point's stretch and shear forces 
 	for(i = 0; i < SIZEV; i++) 
 	{ 
@@ -47,8 +30,7 @@ void ClothMesh::DealMesh(void)
 			// For special situations, we need special ways to solute it 
 			if(i == 0) 
 			{ 
-				// While the point is in first row, 
-				// the adjacent point will stretched it in second row 
+				// While the point is in first row, the adjacent point will stretched it in second row 
 				vLength = m_vPosition[i + 1][j] - m_vPosition[i][j]; 
 				fTmp = vLength.Length() - fLength; 
 				vLength.Unit(); 
@@ -82,14 +64,12 @@ void ClothMesh::DealMesh(void)
 					fTmp = vLength.Length() - fWidth; 
 					vLength.Unit(); 
 					m_vStretch[i][j] += vLength * fTmp * QUOTIETY; 
- 
 					// While the point is in both first row and last column, 
 					// it will be sheared by the only point that is in second row and last but one column 
 					vLength = m_vPosition[i + 1][j - 1] - m_vPosition[i][j]; 
 					fTmp = vLength.Length() - fCross; 
 					vLength.Unit(); 
 					m_vShear[i][j] = vLength * fTmp * QUOTIETY; 
- 
 					// Computer normal vector of the point 
 					vNormal = Cross(m_vPosition[i + 1][j] - m_vPosition[i][j], m_vPosition[i][j - 1] - m_vPosition[i][j]); 
 					vNormal.Unit(); 
@@ -302,7 +282,6 @@ void ClothMesh::DealMesh(void)
 					fTmp = vLength.Length() - fCross; 
 					vLength.Unit(); 
 					m_vShear[i][j] += vLength * fTmp * QUOTIETY; 
- 
 					// Computer normal vector of the point 
 					vNormal = Cross(m_vPosition[i][j + 1] - m_vPosition[i][j], m_vPosition[i + 1][j] - m_vPosition[i][j]); 
 					vNormal += Cross(m_vPosition[i + 1][j] - m_vPosition[i][j], m_vPosition[i][j - 1] - m_vPosition[i][j]); 
@@ -314,7 +293,4 @@ void ClothMesh::DealMesh(void)
 			} 
 		} 
 	} 
-}</pre>
-<script src="/inc/gg_read2.js"></script><BR>
-<script src="http://s117.cnzz.com/stat.php?id=1236358&web_id=1236358&show=pic" language="JavaScript" charset="gb2312"></script>
-</body></html>
+}
--- trunk/flags/main.cpp
+++ trunk/flags/main.cpp
@@ -1,37 +1,15 @@
-<html><head><title>SimCloth.zip SimCloth_main.cpp</title>
-<LINK href="/inc/read_style.css" type=text/css rel=stylesheet></head>
-<body>
-<p><a href=http://www.pudn.com>www.pudn.com</a> &gt; <a href="http://www.pudn.com/downloads78/sourcecode/graph/detail298391.html">SimCloth.zip</a> &gt SimCloth_main.cpp, change:2006-11-28,size:4530b</p><!-- saved from http://www.pudn.com -->
-<script src="/inc/gg_read1.js"></script><BR>
-<pre name="code" class="cpp">
-/* ========================================================================= * 
- *	Name:		Cloth_main.cpp                                               * 
- *	Function:	The main program.                                            * 
- *	Date:		2006-11-22                                                   * 
- *	Editor:		jixing_alex                                                  * 
- * ========================================================================= */ 
- 
-// Include 
 #include <iostream> 
-#include <conio.h> 
 #include <cmath> 
 #include <GL/glut.h> 
- 
-// Include my file 
-#include "ClothMesh.h" 
- 
-// Declaration of globe variable 
-ClothMesh g_Cloth; 
- 
-// Declaration of function 
-GLvoid SetupRC(GLvoid); 
-GLvoid ReShapeFunc(GLsizei, GLsizei); 
-GLvoid DisplayFunc(GLvoid); 
-GLvoid KeyboardFunc(unsigned char, int, int); 
-GLvoid TimerFunc(int); 
-GLvoid DrawMesh(GLvoid); 
- 
-// main 
+#include <GL/gl.h> 
+#include "flag.h" 
+ClothMesh g_Cloth;
+void SetupRC(void);
+void ReShapeFunc(GLsizei, GLsizei); 
+void DisplayFunc(void);
+void KeyboardFunc(unsigned char, int, int); 
+void TimerFunc(int); 
+void DrawMesh(void); 
 int main(int argc, char **argv) 
 { 
 	glutInit(&argc, argv); 
@@ -47,9 +25,7 @@ int main(int argc, char **argv)
 	glutMainLoop(); 
 	return 0; 
 } 
- 
-// Get ready before render. It include set light, material, backgroud, ... 
-GLvoid SetupRC(GLvoid) 
+void SetupRC(void) 
 { 
     GLfloat	lightPos[] = { 100.f, 0.0f, 100.0f, 1.0f }; 
 	GLfloat spotDir[] = { 0.0f, 0.0f, -1.0f }; 
@@ -57,11 +33,9 @@ GLvoid SetupRC(GLvoid)
     GLfloat diffuseLight[] = { 0.7f, 0.7f, 0.7f, 1.0f }; 
     GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
     GLfloat specref[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
- 
     glEnable(GL_DEPTH_TEST); 
     glFrontFace(GL_CCW); 
     glEnable(GL_NONE); 
- 
     glEnable(GL_LIGHTING); 
     glLightfv(GL_LIGHT0,GL_POSITION,lightPos); 
 	glLightfv(GL_LIGHT0,GL_SPOT_DIRECTION,spotDir); 
@@ -69,22 +43,17 @@ GLvoid SetupRC(GLvoid)
     glLightfv(GL_LIGHT0,GL_DIFFUSE,diffuseLight); 
     glLightfv(GL_LIGHT0,GL_SPECULAR, specular); 
     glEnable(GL_LIGHT0); 
- 
     glEnable(GL_COLOR_MATERIAL); 
     glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE); 
 	glShadeModel(GL_SMOOTH); 
- 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f ); 
 } 
- 
-// Regulate viewport's size when the window was modified. 
 GLvoid ReShapeFunc(GLsizei w, GLsizei h) 
 { 
 	if(w >= h) 
 		glViewport((w - h) / 2, 0, h, h); 
 	else 
 		glViewport(0, (h - w) / 2, w, w); 
- 
 	glMatrixMode(GL_PROJECTION); 
 	glLoadIdentity(); 
 	gluPerspective(60.0, 1, -200.0, 200.0); 
@@ -92,34 +61,25 @@ GLvoid ReShapeFunc(GLsizei w, GLsizei h)
 	glMatrixMode(GL_MODELVIEW); 
 	glLoadIdentity(); 
 } 
- 
-// This include the thing that will be rendered. 
-GLvoid DisplayFunc(GLvoid) 
+void DisplayFunc(void) 
 { 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 	DrawMesh(); 
 	glutSwapBuffers(); 
 } 
- 
-// Key's management 
 GLvoid KeyboardFunc(unsigned char key, int x, int y) 
 { 
-	if(key == ' ') 
+	if(key == 'd') 
 		g_Cloth.DealMesh(); 
- 
 	glutPostRedisplay(); 
 } 
- 
-// Time's management in animation 
 GLvoid TimerFunc(int value) 
 { 
 	g_Cloth.DealMesh(); 
 	glutPostRedisplay(); 
 	glutTimerFunc(0, TimerFunc, 1); 
 } 
- 
-// Draw cloth's mesh 
-GLvoid DrawMesh(GLvoid) 
+void DrawMesh(void) 
 { 
 	for(int i = 0; i < SIZEV - 1; i++) 
 	{ 
@@ -154,7 +114,4 @@ GLvoid DrawMesh(GLvoid)
 			glEnd(); 
 		} 
 	} 
-}</pre>
-<script src="/inc/gg_read2.js"></script><BR>
-<script src="http://s117.cnzz.com/stat.php?id=1236358&web_id=1236358&show=pic" language="JavaScript" charset="gb2312"></script>
-</body></html>
+}
